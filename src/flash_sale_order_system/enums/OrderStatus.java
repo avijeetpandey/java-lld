141049fef3d7abcccd05bcbd4aca6c9e60ae1915
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-02-26 21:02:45 +0530

    add: roles

---
+++ src/flash_sale_order_system/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package flash_sale_order_system.enums;
+
+public enum OrderStatus {
+    SUCCESS,
+    FAILED,
+    PENDING
+}
---
+++ src/flash_sale_order_system/enums/Role.java
@@ -0,0 +1,6 @@
+package flash_sale_order_system.enums;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN
+}
